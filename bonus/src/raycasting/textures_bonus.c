a85466e8b77be68b8ffe38cbf26c7e9d4da9d0eb
berila7 <berilasimo7@gmail.com>
2025-09-15 20:14:32 +0100

    remove unload function

--- bonus/src/raycasting/textures_bonus.c
+++ bonus/src/raycasting/textures_bonus.c
@@ -31,15 +31,6 @@ int load_textures(void)
 	return 0;
 }
 
-void unload_textures(void)
-{
-	t_data *d = data();
-	if (d->no_tex) mlx_delete_texture(d->no_tex), d->no_tex = NULL;
-	if (d->so_tex) mlx_delete_texture(d->so_tex), d->so_tex = NULL;
-	if (d->we_tex) mlx_delete_texture(d->we_tex), d->we_tex = NULL;
-	if (d->ea_tex) mlx_delete_texture(d->ea_tex), d->ea_tex = NULL;
-}
-
 static inline mlx_texture_t *pick_wall_texture(const t_ray *ray)
 {
 	t_data *d = data();
@@ -63,7 +54,7 @@ static inline mlx_texture_t *pick_wall_texture(const t_ray *ray)
 static inline uint32_t sample_texel_rgba(mlx_texture_t *tex, int x, int y)
 {
 	const uint8_t *p = tex->pixels + (y * tex->width + x) * tex->bytes_per_pixel;
-	return pack_rgba(p[0], p[1], p[2], p[3]); // MLX42 stores RGBA
+	return pack_rgba(p[0], p[1], p[2], p[3]);
 }
 
 static inline int compute_tex_x(const t_ray *ray, mlx_texture_t *tex)
@@ -77,7 +68,6 @@ static inline int compute_tex_x(const t_ray *ray, mlx_texture_t *tex)
 
 	int tx = (int)(offset / (double)TILE_SIZE * (double)tex->width);
 
-	// Flip horizontally to make orientation feel natural.
 	if (ray->was_vert && !ray->is_right)
 		tx = (int)tex->width - 1 - tx;
 	else if (!ray->was_vert && ray->is_down)
@@ -92,7 +82,6 @@ static inline int compute_tex_x(const t_ray *ray, mlx_texture_t *tex)
 }
 
 
-// Draw one textured vertical column at screen x using the ray result.
 void render_textured_column(const t_ray *ray, int screen_x, double line_h)
 {
 	int y;
@@ -103,7 +92,6 @@ void render_textured_column(const t_ray *ray, int screen_x, double line_h)
 	mlx_texture_t *tex = pick_wall_texture(ray);
 	if (!tex) return;
 
-	// Compute projected wall start/end (float for precision)
 	double wall_top_f = (WINDOW_H / 2.0) - (line_h / 2.0);
 	double wall_bot_f = wall_top_f + line_h;
 
@@ -112,7 +100,6 @@ void render_textured_column(const t_ray *ray, int screen_x, double line_h)
 	if (wall_top < 0) wall_top = 0;
 	if (wall_bottom > WINDOW_H) wall_bottom = WINDOW_H;
 
-	// Ceiling
 	y = 0;
 	while (y < wall_top)
 	{
@@ -120,10 +107,8 @@ void render_textured_column(const t_ray *ray, int screen_x, double line_h)
 		y++;
 	}
 
-	// Texture x coordinate is constant for this column
 	tx = compute_tex_x(ray, tex);
 
-	// Draw wall section
 	y = wall_top;
 	while (++y < wall_bottom)
 	{
@@ -137,7 +122,6 @@ void render_textured_column(const t_ray *ray, int screen_x, double line_h)
 		draw_pixel(screen_x, y, color);
 	}
 
-	// Floor
 	y = wall_bottom;
 	while (++y < WINDOW_H)
 		draw_pixel(screen_x, y, data()->floor);
